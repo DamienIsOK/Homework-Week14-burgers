ccfb541dd1291092a0752c7059cd97edafade4f4
Damien Wright <damien@Damiens-MacBook-Pro.local>
2016-10-02 21:44:46 -0700

    in prog: handlebars layout are being separated into 3 divs

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-var mysql      = require('mysql');
+/*var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
@@ -11,8 +11,10 @@ var query = connection.query('INSERT INTO veggies (veggie_name) VALUES ?', [vegg
 		if (error) throw error;
 		console.log(result);
 	});
+*/
 
 
--- for(var i = 0; i < veggies_arr; i++) {
--- 	INSERT INTO veggies (veggie_name) VALUES (veggies_arr[i]);
--- };
+INSERT INTO veggies_db.veggies (veggie_name) VALUES ('asparagus');
+INSERT INTO veggies_db.veggies (veggie_name) VALUES ('brussels sprouts');
+INSERT INTO veggies_db.veggies (veggie_name) VALUES ('tomatoes');
+INSERT INTO veggies_db.veggies (veggie_name) VALUES ('cucumbers');
